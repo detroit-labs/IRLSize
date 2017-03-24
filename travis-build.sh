2185aac47cca24eb9061e6e632c8341148f4c250
Jeff Kelley <SlaunchaMan@gmail.com>
2017-03-24 09:57:40 -0400

    Use build script instead of Travis matrix.

---
+++ travis-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -ev
+
+rc=0
+
+for OS in 10.0 10.1 10.2; do 
+	for device in "iPhone SE" "iPhone 7" "iPhone 7 Plus" "iPad Pro (9.7 inch)" "iPad Pro (12.9 inch)"; do
+		xcodebuild -workspace "Example/IRLSizeExample.xcworkspace" \
+		           -scheme "IRLSizeExample" \
+		           -destination "name=${device},OS=${OS},platform=iOS Simulator" \
+		           -configuration Debug test | xcpretty -c;
+
+		((rc += $?))
+	done
+done
+
+if [ $rc == 0 ]; then
+	pod lib lint
+	((rc += $?))
+fi
+
+exit $rc
