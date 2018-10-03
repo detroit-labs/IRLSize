7c61e98be44b5a16e54a52643def4004c1310cb3
Jeff Kelley <SlaunchaMan@gmail.com>
2018-10-03 15:52:09 -0400

    Added 40mm and 44mm Apple Watch models

--- Pod/Classes/watchOS/WKInterfaceDevice+IRLSize.m
+++ Pod/Classes/watchOS/WKInterfaceDevice+IRLSize.m
@@ -14,11 +14,17 @@
 // https://www.sven.de/dpi/ is a good resource for determining screen sizes.
 static const float kAppleWatch38mmScreenWidth = 0.0212f;
 static const float kAppleWatch38mmScreenHeight = 0.0265f;
+static const float kAppleWatch40mmScreenWidth = 0.029f;
+static const float kAppleWatch40mmScreenHeight = 0.0353f;
 static const float kAppleWatch42mmScreenWidth = 0.0243f;
 static const float kAppleWatch42mmScreenHeight = 0.0304f;
+static const float kAppleWatch44mmScreenWidth = 0.0322f;
+static const float kAppleWatch44mmScreenHeight = 0.0393f;
 
 static const NSUInteger kAppleWatch38mmHeightPoints = 170;
+static const NSUInteger kAppleWatch40mmHeightPoints = 197;
 static const NSUInteger kAppleWatch42mmHeightPoints = 195;
+static const NSUInteger kAppleWatch44mmHeightPoints = 224;
 
 @implementation WKInterfaceDevice (IRLSize)
 
@@ -33,10 +39,20 @@ static const NSUInteger kAppleWatch42mmHeightPoints = 195;
             estimatedDimensions.width = kAppleWatch38mmScreenWidth;
             estimatedDimensions.height = kAppleWatch38mmScreenHeight;
             break;
+
+        case kAppleWatch40mmHeightPoints:
+            estimatedDimensions.width = kAppleWatch40mmScreenWidth;
+            estimatedDimensions.height = kAppleWatch40mmScreenHeight;
+            break;
             
         case kAppleWatch42mmHeightPoints:
             estimatedDimensions.width = kAppleWatch42mmScreenWidth;
             estimatedDimensions.height = kAppleWatch42mmScreenHeight;
+            break;
+
+        case kAppleWatch44mmHeightPoints:
+            estimatedDimensions.width = kAppleWatch44mmScreenWidth;
+            estimatedDimensions.height = kAppleWatch44mmScreenHeight;
             break;
     }
     
