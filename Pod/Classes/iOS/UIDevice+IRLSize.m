bc9d15ed70df4d23297476398b9f8464dc93fee2
Jeff Kelley <SlaunchaMan@gmail.com>
2018-11-01 14:06:43 -0400

    Updated screen dimensions from official Apple source.

--- Pod/Classes/iOS/UIDevice+IRLSize.m
+++ Pod/Classes/iOS/UIDevice+IRLSize.m
@@ -12,23 +12,23 @@
 
 #import "UIView+IRLSizePrivate.h"
 
-// https://www.sven.de/dpi/ is a good resource for determining screen sizes.
+// https://developer.apple.com/accessories/Accessory-Design-Guidelines.pdf
 
 // iPhone/iPod touch
 static const float kiPhone3_5InchScreenHeight = 0.0740f;
 static const float kiPhone3_5InchScreenWidth = 0.0493f;
-static const float kiPhone4_0InchScreenHeight = 0.0885f;
-static const float kiPhone4_0InchScreenWidth = 0.0499f;
-static const float kiPhone4_7InchScreenHeight = 0.1041f;
-static const float kiPhone4_7InchScreenWidth = 0.0585f;
-static const float kiPhone5_5InchScreenHeight = 0.1218f;
-static const float kiPhone5_5InchScreenWidth = 0.0685f;
-static const float kiPhone5_8InchScreenHeight = 0.1337f;
-static const float kiPhone5_8InchScreenWidth = 0.0618f;
-static const float kiPhone6_1InchScreenHeight = 0.1407f;
-static const float kiPhone6_1InchScreenWidth = 0.065f;
-static const float kiPhone6_5InchScreenHeight = 0.1499f;
-static const float kiPhone6_5InchScreenWidth = 0.0693f;
+static const float kiPhone4_0InchScreenHeight = 0.09039f;
+static const float kiPhone4_0InchScreenWidth = 0.05170f;
+static const float kiPhone4_7InchScreenHeight = 0.10405f;
+static const float kiPhone4_7InchScreenWidth = 0.05850f;
+static const float kiPhone5_5InchScreenHeight = 0.12154f;
+static const float kiPhone5_5InchScreenWidth = 0.06836f;
+static const float kiPhone5_8InchScreenHeight = 0.13575f;
+static const float kiPhone5_8InchScreenWidth = 0.06312f;
+static const float kiPhone6_1InchScreenHeight = 0.13978f;
+static const float kiPhone6_1InchScreenWidth = 0.06458f;
+static const float kiPhone6_5InchScreenHeight = 0.14971f;
+static const float kiPhone6_5InchScreenWidth = 0.06961f;
 
 static const NSUInteger kiPhone3_5InchHeightPoints = 480;
 static const NSUInteger kiPhone4_0InchHeightPoints = 568;
@@ -40,14 +40,14 @@ static const NSUInteger kiPhone5_8InchHeightPoints = 812;
 static const NSUInteger kiPhone6_1InchHeightPoints = 896;
 
 // iPad
-static const float kiPad7_9InchScreenHeight = 0.1605f;
-static const float kiPad7_9InchScreenWidth = 0.1204f;
-static const float kiPad9_7InchScreenHeight = 0.1971f;
-static const float kiPad9_7InchScreenWidth = 0.1478f;
-static const float kiPad10_5InchScreenHeight = 0.2134f;
-static const float kiPad10_5InchScreenWidth = 0.16f;
-static const float kiPad12_9InchScreenHeight = 0.2622f;
-static const float kiPad12_9InchScreenWidth = 0.1965f;
+static const float kiPad7_9InchScreenHeight = 0.16124f;
+static const float kiPad7_9InchScreenWidth = 0.12131f;
+static const float kiPad9_7InchScreenHeight = 0.19647f;
+static const float kiPad9_7InchScreenWidth = 0.14739f;
+static const float kiPad10_5InchScreenHeight = 0.21350f;
+static const float kiPad10_5InchScreenWidth = 0.16013f;
+static const float kiPad12_9InchScreenHeight = 0.26227f;
+static const float kiPad12_9InchScreenWidth = 0.19661f;
 
 static const NSUInteger kiPadHeightPoints = 1024;
 static const NSUInteger kiPadPro10Dot5InchHeightPoints = 1112;
